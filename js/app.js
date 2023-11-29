7dc6a196352ea5a30879187cd2628ccdd933b360
Elniel <corgoo7z49h@gmail.com>
2023-11-29 23:42:22 +0200

    Add interface

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+
+
+const screen = document.getElementById('screen');
+
+
+// keys
+const plus = document.getElementById('plus');
+const subtract = document.getElementById('subtract');
+const multiple = document.getElementById('multiple');
+const divide = document.getElementById('divide');
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const none = document.getElementById('none');
+const zero = document.getElementById('zero');
+
+const doit = document.getElementById('doit');
+const del = document.getElementById('delete');
+const equal = document.getElementById('equal');
+
+
+
+
